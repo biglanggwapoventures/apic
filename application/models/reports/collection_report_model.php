353ddd5a3b5848c80ab979764cefdca86984a5a7
adriannatabio <natabioadr@gmail.com>
2016-04-22 12:52:52 +0800

    modification in collection report
    days collected refactored from “current date minus receipt date” to
    “current date minus deposit date”

--- application/models/reports/collection_report_model.php
+++ application/models/reports/collection_report_model.php
@@ -4,7 +4,7 @@ class Collection_report_model extends CI_Model
 {
 	public function generate($customer = FALSE, $agent = FALSE, $start_date = FALSE, $end_date = FALSE)
 	{
-		$this->db->select('receipt.id, receipt_detail.fk_sales_delivery_id, receipt_detail.amount, customer.company_name AS customer_name, DATEDIFF(CURDATE(), delivery.date) AS days_collected', FALSE)
+		$this->db->select('receipt.id, receipt_detail.fk_sales_delivery_id, receipt_detail.amount, customer.company_name AS customer_name, DATEDIFF(receipt.deposit_date, DATE(delivery.date)) AS days_collected', FALSE)
 			->from('sales_receipt_detail AS receipt_detail')
 			->join('sales_receipt AS receipt', 'receipt.id = receipt_detail.fk_sales_receipt_id')
 			->join('sales_customer AS customer', 'customer.id = receipt.fk_sales_customer_id')
@@ -19,10 +19,10 @@ class Collection_report_model extends CI_Model
 			$this->db->where('s_order.fk_sales_agent_id', $agent);
 		}
 		if($start_date){
-			$this->db->where('receipt.`date` >=', $start_date);
+			$this->db->where('receipt.deposit_date >=', $start_date);
 		}
 		if($end_date){
-			$this->db->where('receipt.`date` <=', $end_date);
+			$this->db->where('receipt.deposit_date <=', $end_date);
 		}
 		return $this->db->get()->result_array();
 	}
