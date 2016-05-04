fe3f2a8a6e70898684332ae7951cad0134467195
adriannatabio <natabioadr@gmail.com>
2016-05-04 13:38:18 +0800

    improvements in counter receipt printing
    improvements in counter receipt printing

--- application/views/printables/sales/counter-receipt.php
+++ application/views/printables/sales/counter-receipt.php
@@ -12,7 +12,8 @@
                 margin-top:-20px;
             }
             .content{
-                width:45%;
+                width:3.6in;
+                overflow;hidden;
             }
            
 
@@ -21,23 +22,14 @@
     </head>
     <body class="content">
         <style type="text/css">
-         .customer-info{
-                padding: 0 0 0 10px;
-            }
+         
             .products table thead tr td{
                 border:0!important;
                 padding-top:7px;
                 padding-bottom: 6px;
             }
-            .products table td:nth-child(6){
+            .products table td:nth-child(3){
                 text-align: right;
-            }
-             .products table td:nth-child(4){
-                text-align: right;
-            }
-            .products table td{
-                border:0;
-                padding: 2px auto;
             }
             #footer {
                 bottom:0px;
@@ -49,19 +41,21 @@
             }
             table td{
                 font-weight: bold;
-                padding:0 5px;
+            }
+            .products table td{
+                font-size:12px;
             }
             #row-end,.labels{
                 font-weight:normal!important;
             }</style>
-        <?php $maxDetailsCount = 14; ?>
+        <?php $maxDetailsCount = 10; ?>
         <?php $detailsCursor = 0; ?>
         <?php $detailsCount = count($details['details']); ?>
         <?php $tempDetailsCount = $detailsCount; ?>
         <?php $pageTotal = 0; ?>
 
         <?php
-            $pageTotal = (int)ceil($detailsCount / 14);
+            $pageTotal = (int)ceil($detailsCount / $maxDetailsCount);
         ?>
 
         <?php $pageCounter = 1; ?>
@@ -69,7 +63,7 @@
         <?php $total = 0;?>
         <?php while ($pageCounter <= $pageTotal): ?>
             <?php if ($pageCounter > 1): ?>
-                <?php $margin = "style='margin-top:20px;'" ?>
+                <?php $margin = "style='margin-top:35px;'" ?>
             <?php endif; ?>
             <div style="height:5in;">
                 <div class="row header" <?= $margin ?>> 
@@ -81,9 +75,9 @@
                         <small>Page <?= "{$pageCounter}/{$pageTotal}" ?></small>
                     </div>
                 </div> 
-                <div class="row customer" style='border:1px solid #000;border-radius: 5px;margin-top:5px'>
+                <div class="row customer" style='border-top:1px solid #000;border-bottom:1px solid #000;margin-top:5px'>
                     <div class="col-xs-12">
-                        <table style="width:100%">
+                        <table style="width:100%;">
                             <tbody>
                                 <tr>
                                     <td class="labels">CUSTOMER</td>
@@ -97,16 +91,25 @@
                                         <strong><?= $details['customer_address'] ?></strong>
                                     </td>
                                 </tr>
+                                 <tr>
+                                    <td class="labels">CREDIT TERMS</td>
+                                    <td class='customer-info' colspan="3">
+                                        <strong><?= $details['credit_term'] ?> day/s</strong>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
                 <div class='row products'>
                     <div class='col-xs-12'>
-                        <table style="width:100%" >
+                        <table style="width:100%;" >
                             <thead>
                                 <tr>
-                                    <td>DATE</td><td>DUE DATE</td><td>SI#</td><td>PRICE</td><td>KG/S</td><td>AMOUNT</td>
+                                    <td class="text-center" style="width:30%">DATE</td>
+                                    <td class="text-center" style="width:10%">SI#</td>
+                                    <td class="text-right"  style="width:25%">AMOUNT</td>
+                                    <td class="text-center" style="width:35%">DUE DATE</td>
                                 </tr>
                             </thead>
                             <tbody>
@@ -114,18 +117,22 @@
                                     <?php if (isset($details['details'][$detailsCursor])): ?>
                                         <tr>
                                             <?php $pl_date =  date_create($details['details'][$detailsCursor]['date']); ?>
-                                            <td><?= $pl_date->format('m/d/Y') ?></td>
-                                            <td><?= $pl_date->modify("+ {$details['credit_term']} day")->format('m/d/Y') ?></td>
-                                            <td><?= $details['details'][$detailsCursor]['invoice_number'] ?></td>
-                                            <td><?= number_format($details['details'][$detailsCursor]['unit_price'], 2) ?></td>
-                                            <td><?= number_format($details['details'][$detailsCursor]['quantity'], 2) ?></td>
+                                            <td class="text-center"><?= $pl_date->format('m/d/Y') ?></td>
+                                            <td class="text-center"><?= $details['details'][$detailsCursor]['invoice_number'] ?></td>
                                             <?php $amount = $details['details'][$detailsCursor]['quantity'] * $details['details'][$detailsCursor]['unit_price']?>
-                                            <td><?= number_format($amount, 2) ?></td>
+                                            <td class="text-right"><?= number_format($amount, 2) ?></td>
+                                            <td class="text-center"><?= $pl_date->modify("+ {$details['credit_term']} day")->format('m/d/Y') ?></td>
                                             <?php $total += $amount;?>
                                         </tr>
                                         <?php if ($detailsCursor+1 == $detailsCount): ?>
                                             <tr><td colspan="6" class="text-center"><small>***NOTHING FOLLOWS***</small></td></tr>
-                                            <tr ><td colspan="4"></td><td style="border:1px solid black;border-right: 0;">TOTAL</td><td class="text-right" style="border:1px solid black;border-left: 0"><?= number_format($total, 2)?></td></tr>
+                                            <tr >
+                                                <td colspan="2"></td>
+                                                <td style="border:1px solid black;border-right: 0;padding:0">TOTAL</td>
+                                                <td style="border:1px solid black;border-left: 0;padding:0;text-align:center!important;">
+                                                    <?= number_format($total, 2)?>
+                                                </td>
+                                            </tr>
                                         <?php endif; ?>
                                         <?php $detailsCursor++; ?>
                                     <?php else: ?>
